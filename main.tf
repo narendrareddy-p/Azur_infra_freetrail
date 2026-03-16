1d29a2c8376b35c4abb5f5129ab5c523d01ebe24
narendrareddy-p <narendra.palakolanu30@gmail.com>
2026-03-16 16:16:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ provider "azurerm" {
 module "resource_group" {
 
   source = "git::https://github.com/narendrareddy-p/modules.git//resource-group?ref=feature"
-  resource_group_name  = var.resource_group_name
+  resource_group_name = var.resource_group_name
   location = var.location
 }
