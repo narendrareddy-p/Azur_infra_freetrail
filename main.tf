4277e8d2bca1e5ee624f6dd7bdb00b5a71ed35d9
narendrareddy-p <narendra.palakolanu30@gmail.com>
2026-03-16 16:30:57 +0530

    Update resource group module configurations

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ provider "azurerm" {
 module "resource_group" {
 
   source = "git::https://github.com/narendrareddy-p/modules.git//resource-group?ref=feature"
-  resource_group_name = var.resource_group_name
-  location = var.location
+  resource_group_name = "rg-dev-demo"
+  location            = "eastus"
+}
+
+module "resource_group_prod" {
+
+  source = "git::https://github.com/narendrareddy-p/modules.git//resource-group?ref=feature"
+  resource_group_name = "rg-prod-demo"
+  location            = "eastus"
 }
