97968e53a7e4d3f449cd2800ddbdc9b6afc7606a
Narendra Reddy Palakolanu <narendra.palakolanu@valuemomentum.com>
2026-03-17 13:49:54 +0530

    added aks infra

--- main.tf
+++ main.tf
@@ -16,3 +16,19 @@ module "resource_group_prod" {
   location            = "eastus"
 }
 */
+
+
+module "network" {
+  source   = "git::https://github.com/narendrareddy-p/modules.git//virtual-network?ref=feature"
+  vnet_name = aks-vnet
+  location  = eastus
+   resource_group_name  = rg-dev-demo
+}
+
+module "aks" {
+  source    = "git::https://github.com/narendrareddy-p/modules.git//aks?ref=feature"
+  aks_name  = dev-aks
+  location  = eastus
+  resource_group_name   = rg-dev-demo
+  subnet_id = module.network.subnet_id
+}
