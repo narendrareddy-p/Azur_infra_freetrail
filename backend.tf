31e9cab69f887ab881daaa54d3a94bee61d3099d
Narendra Reddy Palakolanu <narendra.palakolanu@valuemomentum.com>
2026-03-17 10:51:50 +0530

    updated version

--- backend.tf
+++ backend.tf
@@ -1,5 +1,12 @@
 terraform {
 
+    required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
   backend "azurerm" {
 
     resource_group_name  = "rg-tfstate"
