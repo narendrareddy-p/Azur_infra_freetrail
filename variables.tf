c3833f905fb5fab617dc34c601d065cae4589494
Narendra Reddy Palakolanu <narendra.palakolanu@valuemomentum.com>
2026-03-23 12:10:43 +0530

    added

--- variables.tf
+++ variables.tf
@@ -1,7 +1,16 @@
-variable "resource_group_name" {
+variable "rg_name" {
   type = string
 }
 
 variable "location" {
   type = string
+}
+
+variable "resource_groups" {
+  description = "Map of resource groups"
+  type = map(object({
+    name     = string
+    location = string
+    tags     = map(string)
+  }))
 }
