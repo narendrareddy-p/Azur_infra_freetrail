20b35969e790c6b8f82c02b83f8cabf0a3053e4b
Narendra Reddy Palakolanu <narendra.palakolanu@valuemomentum.com>
2026-04-06 11:39:47 +0530

    added

--- RG.tf
+++ RG.tf
@@ -8,3 +8,13 @@ module "resource_group" {
   location = var.location
   tags     = var.tags
 }
+
+module "resource_group_devops" {
+
+  source =     "git::https://github.com/narendrareddy-p/modules.git//resource-group?ref=feature"
+  
+
+  resource_group_name  = "non-prod-devlopers"
+  location = "East US"
+  tags     = var.tags
+}
