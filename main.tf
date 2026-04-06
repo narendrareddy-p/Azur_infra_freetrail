d41ae5ac44051b4c58240454da47a0ec5aae2675
Narendra Reddy Palakolanu <narendra.palakolanu@valuemomentum.com>
2026-04-06 14:52:47 +0530

    added blob container

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+## Resource Group
+
 resource "azurerm_resource_group" "devops" {
   name     = "Devops-infra"
   location = "East US"
@@ -8,7 +10,7 @@ resource "azurerm_resource_group" "devops" {
   }
 }
 
-
+## Storage account
 resource "azurerm_storage_account" "devopsstorage996697" {
   name                     = "devopsstorage996697"
   resource_group_name      = azurerm_resource_group.devops.name
@@ -16,4 +18,18 @@ resource "azurerm_storage_account" "devopsstorage996697" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
+}
+## Container
+resource "azurerm_storage_container" "devops-infra" {
+  name                  = "devops-infra"
+  storage_account_name    = azurerm_storage_account.devopsstorage996697.name
+  container_access_type = "private"
+}
+## Blob
+resource "azurerm_storage_blob" "example" {
+  name                   = "my-awesome-content.zip"
+  storage_account_name   = azurerm_storage_account.devopsstorage996697.name
+  storage_container_name = azurerm_storage_container.devops-infra.name
+  type                   = "Block"
+  source                 = "some-local-file.zip"
 }
