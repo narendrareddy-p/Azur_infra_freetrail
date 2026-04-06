6aafdc78aa711739388a20623df48c022598691f
Narendra Reddy Palakolanu <narendra.palakolanu@valuemomentum.com>
2026-04-06 15:34:37 +0530

    added the subnet

--- main.tf
+++ main.tf
@@ -45,12 +45,13 @@ resource "azurerm_virtual_network" "vnet" {
     resource_group_name = azurerm_resource_group.devops.name
     address_space = ["10.0.1.0/24"]
     tags = azurerm_resource_group.devops.tags
+  
+}
 
-    ##Subnets
-
-    subnet = {
-        name = "infra-devops-dev"
-        address_prefixes = ["10.0.1.0/28"]
-    }
+resource "azurerm_subnet" "subnet" {
+    name = "infra_devops_dev"
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    resource_group_name = azurerm_resource_group.devops.name
+    address_prefixes = ["10.0.1.0/28"]
   
 }
