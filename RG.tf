57145eab59ef5ba334a4b1a9c954c47b0eff478f
Narendra Reddy Palakolanu <narendra.palakolanu@valuemomentum.com>
2026-04-06 11:19:27 +0530

    updated RG

--- RG.tf
+++ RG.tf
@@ -2,9 +2,9 @@
 module "resource_group" {
 
   source =     "git::https://github.com/narendrareddy-p/modules.git//resource-group?ref=feature"
-  for_each = var.resource_groups
+  
 
-resource_group_name   = each.value.rg_name
-  location = each.value.location
-  tags     = each.value.tags
+  resource_group_name  = var.rg_name
+  location = var.location
+  tags     = var.tags
 }
