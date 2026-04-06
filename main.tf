af76e84ff8d34156c5cdf3858440ec5b149b3a04
Narendra Reddy Palakolanu <narendra.palakolanu@valuemomentum.com>
2026-04-06 14:44:24 +0530

    Added the storage account

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ resource "azurerm_resource_group" "devops" {
     Owner = "Nare"
   }
 }
+
+
+resource "azurerm_storage_account" "devops_storage996697" {
+  name                     = "devops_storage996697"
+  resource_group_name      = azurerm_resource_group.devops.name
+  location                 = azurerm_resource_group.devops.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+}
