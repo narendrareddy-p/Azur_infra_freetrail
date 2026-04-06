074eecb0202e95c6556322f2c4e202c76818a573
Narendra Reddy Palakolanu <narendra.palakolanu@valuemomentum.com>
2026-04-06 12:57:45 +0530

    updated the main file with tags

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-resource "azurerm_resource_group" "this" {
+resource "azurerm_resource_group" "devops" {
   name     = "Devops-infra"
   location = "East US"
+  tags = {
+    Environment = "Dev"
+    Project = "Demo"
+    Owner = "Nare"
+  }
 }
