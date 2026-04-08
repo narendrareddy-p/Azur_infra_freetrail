7e14e86165d89cb80e20beda62803c847535921d
Narendra Reddy Palakolanu <narendra.palakolanu@valuemomentum.com>
2026-04-08 14:36:06 +0530

    created the azure keyvault

---
+++ security.tf
@@ -0,0 +1,9 @@
+resource "azurerm_key_vault" "devops" {
+
+    name = "Devops-SRE"
+    resource_group_name = azurerm_resource_group.devops.name
+    location = azurerm_resource_group.devops.location
+    sku_name = "standard"
+    tenant_id = "1e9ee4a6-43c3-4ee4-9e2c-f2c1ff322e3a"
+  
+}
