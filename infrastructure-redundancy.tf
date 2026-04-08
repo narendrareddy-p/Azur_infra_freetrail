4cd4ff083d68df0750ffce4a49cff821377ab967
Narendra Reddy Palakolanu <narendra.palakolanu@valuemomentum.com>
2026-04-08 12:30:53 +0530

    created the availabilty set

--- infrastructure-redundancy.tf
+++ infrastructure-redundancy.tf
@@ -3,6 +3,6 @@ resource "azurerm_availability_set" "devops" {
   resource_group_name = azurerm_resource_group.devops.name
   location = azurerm_resource_group.devops.location
   platform_fault_domain_count = 1
-  platform_update_domain_count = 2
+  platform_update_domain_count = 1
   tags = azurerm_resource_group.devops.tags
 }
